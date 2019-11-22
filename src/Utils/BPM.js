4af289407841196c2bf6340b281fdbb4b1c7d9a8
Lukas Dumberger <lukas.dumberger@dumbergerl.de>
2019-11-22 13:21:59 +0100

    note writer

--- src/Utils/BPM.js
+++ src/Utils/BPM.js
@@ -45,7 +45,7 @@ class BPM{
             }
         });
 
-        return null;
+        return 12;
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -10,12 +10,12 @@ const {Composition, Part, Measure} = require('./MusicXML/Composition');
 //window.Note = Note;
 //window.Composition = Composition;
 //window.Part = Part;
-/*
+
 var Measure1 = new Measure(1);
-var Measure2 = new Measure(2);
+/*var Measure2 = new Measure(2);
 var Measure3 = new Measure(3);
 
-/*Measure1
+Measure1
     .addNote( (new Note()).setDuration(12).setStep("C").setOctave(4) )
     .addNote( (new Note()).setDuration(12).setStep("D").setOctave(4) )
     .addNote( (new Note()).setDuration(24).setStep("E").setOctave(4) )
@@ -28,36 +28,44 @@ Measure2
     .addNote( (new Note()).setDuration(24).setStep("Bb").setOctave(4) )
     .addNote( (new Note()).setDuration(24).setStep("B").setOctave(4) )
     .addNote( (new Note()).setDuration(24).setStep("B#").setOctave(4) )
-    .addNote( (new Note()).setDuration(24).setStep("C").setOctave(5) );*
-
+    .addNote( (new Note()).setDuration(24).setStep("C").setOctave(5) );
+*/
 var Part1 = new Part("Piano von Lukas");
-/*Part1
+Part1
     .addMeasure( Measure1 );
-   // .addMeasure( Measure2 )
-    //.addMeasure( Measure3 );/**
+    /*.addMeasure( Measure2 )
+    .addMeasure( Measure3 );*/
 
 var theMusic = new Composition();
 theMusic.addPart(Part1);    
 
 OSMD.renderMusicXML( theMusic.toMusicXML() );
-*/
+
 var theStream = new NoteStream( new MIDIController());
 var theProcessor = new NoteProcessor();
-
 theStream.registerCallback( note => {theProcessor.pushNote(note); });
+
+
+theStream.registerCallback( note => {
+    note.setDuration( BPM.GET_DURATION_C80140( note ));
+
+    Part1.addNote( note );
+
+    OSMD.renderMusicXML( theMusic.toMusicXML() );
+});
 
 /*
 var DigitalPiano = new MIDIController();
 var Recorder = new MIDIRecorder();
 
-/*
+
 DigitalPiano.initController().then( () => {
 
     Recorder.registerOutput( DigitalPiano.output );
     Recorder.registerNoteEndedEvent( note => {
         console.log("EVENT-BUS-DUMP");
 
-        BPM.CLASSIFY_80_90( note );
+        BPM.GET_DURATION_C80140( note );
 
         Part1.addNote( note );
         OSMD.renderMusicXML( theMusic.toMusicXML() );
