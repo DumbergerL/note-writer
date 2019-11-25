be7fbbab7478baac309e54992822b3bc1548a108
Lukas Dumberger <lukas.dumberger@dumbergerl.de>
2019-11-25 22:19:24 +0100

    add multiple classifications

--- src/Processing/NoteDump.js
+++ src/Processing/NoteDump.js
@@ -17,14 +17,24 @@ class NoteDump{
         console.log("DOWNLOAD THE DUMP!");
         let json = [];
 
-        let kMeans = new KMeans(3);
-        kMeans.setDataset( this._dump ).generateCluster(100);
+        let k2Means = new KMeans(2);
+        let k3Means = new KMeans(3);
+        let k4Means = new KMeans(4);
+        let k5Means = new KMeans(5);
+        
+        k2Means.setDataset( this._dump ).generateCluster(100);
+        k3Means.setDataset( this._dump ).generateCluster(100);
+        k4Means.setDataset( this._dump ).generateCluster(100);
+        k5Means.setDataset( this._dump ).generateCluster(100);
 
 
         this._dump.forEach( (note, index) => {
             if(index === this._dump.length - 1)return;  //ignore last Element
             let noteJSON = note.toJSON();
-            noteJSON.cluster = kMeans.getClusterIdOfRecord(note);
+            noteJSON['cluster_k2'] = k2Means.getClusterIdOfRecord(note);
+            noteJSON['cluster_k3'] = k3Means.getClusterIdOfRecord(note);
+            noteJSON['cluster_k4'] = k4Means.getClusterIdOfRecord(note);
+            noteJSON['cluster_k5'] = k5Means.getClusterIdOfRecord(note);
             json.push( noteJSON );
 
         });
